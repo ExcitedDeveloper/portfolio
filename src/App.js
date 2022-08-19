1d4ce28073f3d4fa7ee324311ffe945f33ea5a1a
Steve Hunley <shunley2000@yahoo.com>
2022-08-18 20:19:59 -0500

    Shell Header and Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css"
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
-  return <div className='App'>Hello World!</div>
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+    </div>
+  )
 }
 
 export default App
