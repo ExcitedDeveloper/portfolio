506591aa3413123250db10dd2154cd8a01b1c563
Steve Hunley <shunley2000@yahoo.com>
2022-08-24 20:21:13 -0500

    Start working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,29 @@ import React, { ReactElement } from 'react'
 
 const Header = (): ReactElement => {
   return (
-    <header>
-      <h1>Portfolio</h1>
+    <header className="primary-header">
+      <div id="header-container" className="container">
+        <a href="#header-container">
+          <img src="images/logo.svg" alt="Manage" />
+        </a>
+        <nav className="primary-navigation">
+          <ul aria-label="Primary" className="nav-list">
+            <li>
+              <a href="#header-container">About</a>
+            </li>
+            <li>
+              <a href="#header-container">Experience</a>
+            </li>
+            <li>
+              <a href="#header-container">Work</a>
+            </li>
+            <li>
+              <a href="#header-container">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="button">Resume</button>
+      </div>
     </header>
   )
 }
