888b4035529a56098252848c9e598b0973611f42
Steve Hunley <shunley2000@yahoo.com>
2022-08-27 19:28:49 -0500

    stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, { ReactElement } from 'react'
-import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 
 function App(): ReactElement {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Main />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,33 @@ import React, { ReactElement } from 'react'
 
 const Header = (): ReactElement => {
   return (
-    <header className="primary-header">
-      <div id="header-container" className="container">
-        <a href="#header-container">
-          <img src="images/logo.svg" alt="Manage" />
-        </a>
-        <nav className="primary-navigation">
-          <ul aria-label="Primary" className="nav-list">
-            <li>
-              <a href="#header-container">About</a>
-            </li>
-            <li>
-              <a href="#header-container">Experience</a>
-            </li>
-            <li>
-              <a href="#header-container">Work</a>
-            </li>
-            <li>
-              <a href="#header-container">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <button className="button">Resume</button>
+    <header>
+      <div id="header-container" className="header-container">
+        <div>
+          <a href="#header-container">
+            <img src="logo192.png" className="header-logo" alt="Home" />
+          </a>
+        </div>
+        <div className="header-filler"></div>
+        <div>
+          <nav className="primary-navigation">
+            <ul aria-label="Primary" className="nav-list">
+              <li>
+                <a href="#header-container">About</a>
+              </li>
+              <li>
+                <a href="#header-container">Experience</a>
+              </li>
+              <li>
+                <a href="#header-container">Work</a>
+              </li>
+              <li>
+                <a href="#header-container">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <button className="button">Resume</button>
+        </div>
       </div>
     </header>
   )
