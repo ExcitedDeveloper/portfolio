deb5bef3f21b4481c9052afef4be33c4e7fc7d3a
Steve Hunley <shunley2000@yahoo.com>
2022-09-16 20:14:25 -0500

    Got scrolling header working except for hamburger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,68 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect } from 'react'
 import SideBar3 from '../SideBar3'
 import './Header.css'
 
+const NAV_HEIGHT = 100 // Nav height in px
+
+enum ScrollDirection {
+  Initial = 0,
+  Up = 1,
+  Down = 2
+}
+
 const Header = (): ReactElement => {
+  // https://codingreflections.com/hide-header-on-scroll-down/
+  useEffect(() => {
+    const doc = document.documentElement
+    const w = window
+
+    let prevScroll = w.scrollY !== 0 ? w.scrollY : doc.scrollTop
+    let curScroll
+    let direction = ScrollDirection.Initial
+    let prevDirection = ScrollDirection.Initial
+
+    const header = document.getElementById('header-main')
+
+    if (header == null) return
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const checkScroll = function () {
+      curScroll = w.scrollY !== 0 ? w.scrollY : doc.scrollTop
+      if (curScroll > prevScroll) {
+        // scrolled up
+        direction = ScrollDirection.Up
+      } else if (curScroll < prevScroll) {
+        // scrolled down
+        direction = ScrollDirection.Down
+      }
+
+      if (direction !== prevDirection) {
+        toggleHeader(direction, curScroll)
+      }
+
+      prevScroll = curScroll
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const toggleHeader = function (direction: number, curScroll: number) {
+      if (direction === ScrollDirection.Down && curScroll > NAV_HEIGHT) {
+        header.classList.add('hide')
+        prevDirection = direction
+      } else if (direction === ScrollDirection.Up) {
+        header.classList.remove('hide')
+        prevDirection = direction
+      }
+    }
+
+    window.addEventListener('scroll', checkScroll)
+
+    return () => {
+      window.removeEventListener('scroll', checkScroll)
+    }
+  }, [])
+
   return (
-    <header className="header-main">
+    <header id="header-main" className="header-main">
       <nav id="header-container" className="header-container">
         <div>
           <a href="#header-container">
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,9 +4,115 @@ const Main = (): ReactElement => {
   return (
     <main>
       <h1 style={{ color: 'white' }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quisquam
-        nemo neque illum, harum cum est adipisci. Ipsa magni voluptates
-        reiciendis, dolorum ex laudantium inventore quia quasi ea sequi alias!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        possimus aperiam mollitia, expedita nemo quis fugit? Laborum earum
+        corporis magni impedit explicabo autem culpa, omnis sit, commodi,
+        deleniti incidunt fugiat accusamus ipsum nulla quibusdam totam.
+        Doloribus quas tenetur repudiandae reprehenderit earum omnis eius aut,
+        porro ipsum! Corporis architecto ex, quae, repellat maxime, omnis nemo
+        amet iusto est soluta quos quo iure accusamus beatae! Saepe est facilis,
+        officia enim iusto corrupti impedit natus tempore inventore pariatur,
+        hic accusantium molestiae quod! Explicabo necessitatibus omnis dolorum
+        facilis exercitationem? Adipisci aliquam eius necessitatibus natus amet
+        eveniet beatae doloremque, facere molestiae ea officia consequuntur,
+        repellat dicta id accusantium atque dolorem ab voluptatibus rerum
+        incidunt esse quia nisi voluptate asperiores! Laborum, mollitia vitae
+        veritatis eius numquam, minus sunt non accusamus maxime nam quo tempora
+        omnis earum! Quisquam officia sequi adipisci distinctio excepturi amet
+        eum tenetur, facilis ex sunt provident similique perferendis
+        repudiandae. Voluptas eaque necessitatibus aut corporis ipsam dolore
+        quod, porro ullam qui doloremque excepturi laboriosam doloribus est,
+        repudiandae totam ad, fuga exercitationem facilis nobis. Neque ab iure
+        odio dicta blanditiis, ratione id consequatur adipisci labore voluptate
+        necessitatibus nam omnis eveniet sequi corrupti obcaecati nihil rerum
+        in? Itaque numquam veritatis inventore, fugiat modi consequatur at saepe
+        laborum optio a hic veniam odio in corporis molestiae dolores! Laborum
+        aperiam consectetur libero esse rerum, aspernatur, blanditiis ipsam
+        ipsum fugiat minima nam? Veritatis, quaerat dolores! Sint inventore
+        esse, qui tempore provident recusandae neque possimus necessitatibus
+        corrupti velit excepturi, expedita molestias sunt quis, hic veritatis
+        exercitationem minima. Quo consequuntur magnam itaque alias cumque
+        aperiam architecto? Omnis et repellendus laudantium perspiciatis officia
+        illo quod deserunt fugit qui nobis odio cupiditate dignissimos excepturi
+        eligendi suscipit assumenda ipsum possimus explicabo, voluptatem maxime
+        ex id? Commodi atque facere modi voluptas non placeat molestias at rem
+        tenetur vero recusandae ratione repellat dolorem, nam suscipit quos,
+        minima assumenda incidunt saepe vel quidem earum blanditiis!
+        Reprehenderit mollitia nemo sapiente modi. Libero reiciendis ipsum modi
+        quo iure quisquam, nostrum perspiciatis facilis eos? Harum cum sapiente
+        neque cumque, error maxime at illo. Officia eum totam facere iure unde
+        harum dolorum porro labore? Laborum, eius! Provident, impedit sunt ea
+        dolorem dignissimos nostrum blanditiis, distinctio illo, error doloribus
+        eligendi enim quis reiciendis molestiae nulla cum nam nisi optio. Omnis
+        laborum quo iure, fugiat placeat enim. Aliquid quibusdam in temporibus
+        ea quod velit numquam quidem adipisci fugit illo error sapiente,
+        laudantium esse. Rem quo ab minima magnam natus saepe iste error debitis
+        aliquam aperiam sequi ipsa amet, facere delectus, illum sunt ut quam
+        odio, architecto sit placeat earum rerum provident. Facere odio
+        reiciendis provident distinctio minus eum, velit quibusdam modi
+        doloremque debitis! Quod aut vero odit ex sit velit ad in asperiores
+        expedita maiores similique sapiente, voluptas dolorem nemo eum non quia,
+        ipsa soluta neque! Provident repellendus cupiditate ducimus accusamus,
+        earum molestias minima illum quos ratione a magni, laudantium rerum
+        neque enim iusto, autem quibusdam natus. Dignissimos repellat harum sed
+        quo porro non voluptas nihil quisquam dicta ab fugit, a quidem
+        laudantium vero optio iste explicabo nesciunt perspiciatis maxime
+        aspernatur dolores eos! Sint fugiat id asperiores nostrum aspernatur
+        cumque natus fugit! Sapiente maiores cum aperiam modi quis distinctio
+        obcaecati praesentium dolor aut consequuntur, consequatur iste dolores
+        repellendus commodi eos unde quas asperiores est repellat? Nostrum,
+        corporis. Ut enim dignissimos fuga et atque? Nulla repellendus aperiam,
+        perferendis nobis amet veniam commodi tempore delectus harum quas
+        architecto? Consequuntur id sint autem repellendus doloribus rerum culpa
+        facilis vero ex harum quibusdam in iure delectus suscipit, animi ut
+        distinctio, est veritatis iste? Dolor possimus earum impedit quas
+        reprehenderit voluptatum magnam hic fuga molestiae assumenda. Assumenda
+        dicta alias mollitia totam sint fuga recusandae, maiores eligendi
+        tempora, nostrum labore eaque modi molestias, unde ut quasi esse quas
+        expedita quos. Iusto, eos quia consectetur exercitationem laudantium
+        molestiae aliquam praesentium, voluptas totam consequuntur molestias
+        magni enim, repudiandae nam odio animi ipsa dolore voluptatem
+        dignissimos. Laboriosam autem sequi neque. Voluptates accusamus iure qui
+        a, non doloribus beatae cum iste praesentium soluta quam. Sit, adipisci
+        placeat! Eius deserunt atque reiciendis perferendis ipsam veniam omnis
+        laudantium nemo maiores ab? Deserunt odio voluptates, dignissimos
+        laborum sunt tenetur sed perspiciatis aspernatur magni. Quaerat earum
+        rem omnis, ullam ipsam itaque aliquam hic delectus ut recusandae qui
+        numquam veniam consectetur quia modi doloremque enim non esse neque
+        autem quod officia, nisi voluptas! Minima maiores ea deserunt iusto
+        beatae, ullam repellendus ex, et rerum esse est reprehenderit dolor
+        quisquam fuga, vitae doloribus. Ut error voluptas blanditiis magni ad
+        cupiditate labore ullam, odio odit facilis possimus est minima vero
+        illum, expedita doloremque officiis natus. Alias soluta facilis eum
+        dignissimos. Voluptatum eligendi, labore consequatur nihil
+        necessitatibus error odit quasi laborum quos praesentium animi aliquam
+        sint, quibusdam blanditiis. Voluptate hic minus commodi omnis
+        voluptates, distinctio impedit eligendi natus blanditiis accusamus
+        quidem facilis sit dolores fugit, adipisci facere quod eveniet. Rerum
+        totam nesciunt quisquam deserunt in sunt itaque exercitationem eveniet
+        architecto atque facilis, placeat aliquam velit! Eius animi aliquam
+        laudantium praesentium magnam dolor eum cupiditate! Fugiat, velit?
+        Debitis dolores quod adipisci optio. Reiciendis, delectus ipsa
+        repellendus, veritatis eos voluptatem non magni aliquid voluptate
+        provident, esse eaque perferendis? Excepturi voluptatibus maiores atque
+        voluptate autem harum similique. Nemo explicabo accusantium error amet
+        nisi officiis, dolorem maiores delectus molestias a animi, veniam
+        dignissimos velit assumenda id? Ipsum voluptas dolores sint error nemo,
+        et dignissimos quaerat mollitia similique, atque ullam voluptatibus
+        sapiente asperiores quia itaque expedita veritatis quisquam consequuntur
+        officia aut sunt corrupti sed quod at? Laboriosam tempore nisi impedit
+        blanditiis ratione labore qui dolor saepe porro rem earum sit cum totam
+        quibusdam autem eius officia dignissimos unde accusantium, non aut quo.
+        Error illum hic minima aut, nostrum velit maiores accusantium sint
+        tenetur mollitia. Possimus doloremque sapiente tenetur et, sit
+        aspernatur nobis consequatur praesentium quod illo totam neque rerum a
+        autem? Veniam eius adipisci similique provident autem officiis delectus
+        molestiae quasi, repudiandae consequatur alias eos vitae consectetur
+        asperiores, beatae quam voluptatum, et quisquam ullam? Saepe vero
+        recusandae consequuntur eaque laudantium optio iure quidem culpa
+        nesciunt. Nisi maiores similique earum quaerat. At obcaecati eveniet
+        animi minus beatae, exercitationem fugit impedit consequatur culpa nam,
+        expedita aspernatur mollitia autem.
       </h1>
     </main>
   )
